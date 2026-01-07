8a55ae537cae112b8b5338ce34d66018de1d26d9
Your Name <you@example.com>
2026-01-07 13:49:13 -0600

    Switch to Docker

--- .podman/run.bat
+++ .podman/run.bat
@@ -8,13 +8,13 @@ IF "%~1"=="" (
 )
 
 REM Get the image name from the file
-set /p IMAGE=<.docker/image_name
+set /p IMAGE=<.podman/image_name
 
 REM Check if the image is already available, and pull if needed
-docker image inspect %IMAGE% >nul 2>&1
+podman image inspect %IMAGE% >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo Image not found locally. Pulling %IMAGE%...
-    docker pull %IMAGE%
+    podman pull %IMAGE%
     IF %ERRORLEVEL% NEQ 0 (
         echo Failed to pull image %IMAGE%.
         exit /b 1
@@ -25,9 +25,9 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 
 REM Copy the run script from the image
-FOR /F %%i IN ('docker create %IMAGE%') DO SET CID=%%i
-docker cp %CID%:/interface.ps1 .interface.ps1 >nul
-docker rm -v %CID% >nul
+FOR /F %%i IN ('podman create %IMAGE%') DO SET CID=%%i
+podman cp %CID%:/.podman/interface.ps1 .interface.ps1 >nul
+podman rm -v %CID% >nul
 
 REM Run the image's interface script
 REM powershell -ExecutionPolicy Bypass -File .interface.ps1 %IMAGE%
